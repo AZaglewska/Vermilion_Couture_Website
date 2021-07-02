26f6c352b726260b3070a0dc8ece2271b39472c0
Anna Żaglewska <web.annazaglewska@gmail.com>
2021-07-02 18:51:02 +0200

    adding ThemeProvider to Router

--- src/routing/Router.js
+++ src/routing/Router.js
@@ -3,15 +3,19 @@ import { BrowserRouter, Switch, Route } from "react-router-dom";
 import MainTemplate from "../templates/MainTemplate";
 import About from "../pages/About";
 import { routes } from "../routes";
+import { ThemeProvider } from "styled-components";
+import { mainTheme } from "../globalStyles/MainTheme";
 const Router = () => {
   return (
     <div>
       <BrowserRouter>
-        <Switch>
-          <MainTemplate>
-            <Route path={routes.about} component={About} />
-          </MainTemplate>
-        </Switch>
+        <ThemeProvider theme={mainTheme}>
+          <Switch>
+            <MainTemplate>
+              <Route path={routes.about} component={About} />
+            </MainTemplate>
+          </Switch>
+        </ThemeProvider>
       </BrowserRouter>
     </div>
   );
