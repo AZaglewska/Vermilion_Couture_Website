0e3ede256357a82966d2343c2351f375b5ced381
Anna Żaglewska <web.annazaglewska@gmail.com>
2021-07-02 18:46:53 +0200

    adding GlobalStyles.js and MainTheme.js

---
+++ src/globalStyles/GlobalStyle.js
@@ -0,0 +1,30 @@
+import { createGlobalStyle } from "styled-components";
+import NeueHansKendrick from "../assets/fonts/NeueHansKendrick-Regular.woff";
+
+const GlobalStyle = createGlobalStyle`
+
+
+@font-face {
+font-family: "Neue Hans Kendrick Regular";
+src: url(${NeueHansKendrick}) format('woff');
+font-style: normal;
+font-weight: normal;
+}
+
+*{
+padding: 0;
+margin: 0;
+box-sizing: border-box;
+}
+
+ body{
+    font-family: 'Neue Hans Kendrick Regular', sans-serif;
+}
+
+ button {
+        cursor: pointer;
+    }
+
+`;
+
+export default GlobalStyle;
---
+++ src/globalStyles/MainTheme.js
@@ -0,0 +1,30 @@
+export const mainTheme = {
+  colors: {
+    lightBeige: "#f1efe9",
+    dark: "#353530",
+    light: "#f7f7f7",
+    goldBeige: "#ecdbba",
+  },
+
+  fontSizes: {
+    xxs: "8px",
+    xs: "12px",
+    s: "16px",
+    m: "20px",
+    l: "24px",
+    xl: "30px",
+    xxl: "50px",
+  },
+
+  fontWeight: {
+    superLight: 100,
+    light: 300,
+    medium: 400,
+    semiBold: 500,
+    bold: 700,
+  },
+
+  fontFamily: {
+    //   mainFont: "Roboto",
+  },
+};
