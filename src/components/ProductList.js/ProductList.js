51eaeb199f8d758117c5a7fe49d55ea7ce171d11
Anna Żaglewska <web.annazaglewska@gmail.com>
2021-07-05 23:01:53 +0200

    mapping corset data

--- src/components/ProductList.js/ProductList.js
+++ src/components/ProductList.js/ProductList.js
@@ -1,7 +1,37 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import "react-multi-carousel/lib/styles.css";
 
-const ProductList = () => {
-  return <div></div>;
+const ProductList = ({ corsetData }) => {
+  return (
+    <ul>
+      {corsetData.map((corsetDataElement) => {
+        const { id, name, images, price, priceSecond, description } =
+          corsetDataElement;
+
+        return (
+          <li key={id}>
+            <Link
+              to={{
+                pathname: `/product/${name.replace(/\s/g, "")}`,
+                state: {
+                  name,
+                  images,
+                  price,
+                  priceSecond,
+                  description,
+                },
+              }}
+            >
+              <div>
+                <img src={images[0]} />
+              </div>
+            </Link>
+          </li>
+        );
+      })}
+    </ul>
+  );
 };
 
 export default ProductList;
