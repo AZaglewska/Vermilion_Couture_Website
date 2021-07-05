9f09d2d9e7f452841b49526d0eb82d164c5a5e9a
Anna Żaglewska <web.annazaglewska@gmail.com>
2021-07-05 19:06:49 +0200

    adding dataTypes

---
+++ src/utils/types.js
@@ -0,0 +1,19 @@
+export const dataTypesObj = {
+  underbustCorset: "underbustCorset",
+  overbustContent: "overbustContent",
+  weddingCorset: "weddingCorset",
+  eveningCorset: "eveningCorset",
+  designerData: "designerData",
+  publicationsData: "publicationsData",
+  questionsAndAnswersData: "questionsAndAnswers",
+};
+
+export const dataTypesArray = [
+  "underbustCorset",
+  "overbustContent",
+  "weddingCorset",
+  "eveningCorset",
+  "designerData",
+  "publicationsData",
+  "questionsAndAnswers",
+];
