e5e1f1bb5f08d7d59bb586b24f36c35ad0d4a62a
Anna Żaglewska <web.annazaglewska@gmail.com>
2021-07-05 13:08:44 +0200

    adding new routes paths

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,4 +1,12 @@
 export const routes = {
   home: "/",
   about: "/o-vermilion",
+  publications: "/publikacje",
+  weddingCollection: "/kolekcja-ślubna",
+  eveningCollection: "/kolekcja-wieczorowa",
+  underbustCollection: "/kolekcja-underbust",
+  overbustCollection: "/kolekcja-overbust",
+  couture: "/couture",
+  contact: "/kontakt",
+  singleProduct: "/product/:productName",
 };
--- src/routing/Router.js
+++ src/routing/Router.js
@@ -1,11 +1,20 @@
 import React from "react";
-import { BrowserRouter, Switch, Route } from "react-router-dom";
 import MainTemplate from "../templates/MainTemplate";
+import Home from "../pages/Home";
 import About from "../pages/About";
+import Publications from "../pages/Publications";
+import EveningCollection from "../pages/EveningCollection";
+import WeddingCollection from "../pages/WeddingCollection";
+import UnderbustCollection from "../pages/UnderbustCollection";
+import OverbustCollection from "../pages/OverbustCollection";
+import Contact from "../pages/Contact";
+import Couture from "../pages/Couture";
+import SingleProduct from "../pages/SingleProduct";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
 import { routes } from "../routes";
 import { ThemeProvider } from "styled-components";
 import { mainTheme } from "../globalStyles/MainTheme";
-import Home from "../pages/Home";
+
 const Router = () => {
   return (
     <div>
@@ -15,6 +24,26 @@ const Router = () => {
             <MainTemplate>
               <Route path={routes.home} component={Home} />
               <Route path={routes.about} component={About} />
+              <Route path={routes.publications} component={Publications} />
+              <Route
+                path={routes.eveningCollection}
+                component={EveningCollection}
+              />
+              <Route
+                path={routes.weddingCollection}
+                component={WeddingCollection}
+              />
+              <Route
+                path={routes.underbustCollection}
+                component={UnderbustCollection}
+              />
+              <Route
+                path={routes.overbustCollection}
+                component={OverbustCollection}
+              />
+              <Route path={routes.couture} component={Couture} />
+              <Route path={routes.contact} component={Contact} />
+              <Route path={routes.singleProduct} component={SingleProduct} />
             </MainTemplate>
           </Switch>
         </ThemeProvider>
