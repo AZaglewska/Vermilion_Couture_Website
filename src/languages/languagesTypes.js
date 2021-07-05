b874be7e1e2e268bfa34e44bd82d813fedf98a2c
Anna Żaglewska <web.annazaglewska@gmail.com>
2021-07-05 10:18:29 +0200

    adding navbar languages types

---
+++ src/languages/languagesTypes.js
@@ -0,0 +1,65 @@
+export const navbarLanguages = {
+  ENG: [
+    {
+      main: "About Vermilion",
+      dropDown: [
+        { name: "About", route: "/about-vermilion" },
+        { name: "Publications", route: "/publications" },
+      ],
+    },
+    {
+      main: " Ready collections",
+      dropDown: [
+        { name: "Wedding", route: "/wedding-collection" },
+        { name: "Evening", route: "/evening-collection" },
+      ],
+    },
+    {
+      main: "Basic",
+      dropDown: [
+        { name: "Underbust", route: "/basic-underbust" },
+        { name: "Overbust", route: "/basic-overbust" },
+      ],
+    },
+    { main: "Go Ready To Wear", route: "/go-ready-to-wear", dropDown: [] },
+
+    {
+      main: "Contact",
+      route: "/contact",
+      dropDown: [],
+    },
+  ],
+
+  PL: [
+    {
+      main: "O Vermilion",
+      dropDown: [
+        { name: "O nas", route: "/o-vermilion" },
+        { name: "Publikacje", route: "/publikacje" },
+      ],
+    },
+    {
+      main: "Gotowe kreacje",
+      dropDown: [
+        { name: "Ślubne", route: "/kolekcja-ślubna" },
+        { name: "Wieczorowe", route: "/kolekcja-wieczorowa" },
+      ],
+    },
+
+    {
+      main: "Kolekcja podstawowa",
+      dropDown: [
+        { name: "Underbust", route: "/basic-underbust" },
+        { name: "Overbust", route: "/basic-overbust" },
+      ],
+    },
+
+    { main: "Go Ready To Wear", route: "/go-ready-to-wear", dropDown: [] },
+
+    {
+      main: "Kontakt",
+      route: "/kontakt",
+      dropDown: [],
+    },
+  ],
+};
