b52b8a09fcfadad7e9664108f99eb62f52b52998
Anna Żaglewska <web.annazaglewska@gmail.com>
2021-07-05 14:02:29 +0200

    adding eng routes paths

--- src/routing/Router.js
+++ src/routing/Router.js
@@ -24,9 +24,15 @@ const Router = () => {
             <MainTemplate>
               <Route path={routes.home} component={Home} />
               <Route path={routes.about} component={About} />
+              <Route path={routes.aboutENG} component={About} />
               <Route path={routes.publications} component={Publications} />
+              <Route path={routes.publicationsENG} component={Publications} />
               <Route
                 path={routes.eveningCollection}
+                component={EveningCollection}
+              />
+              <Route
+                path={routes.eveningCollectionENG}
                 component={EveningCollection}
               />
               <Route
@@ -34,15 +40,29 @@ const Router = () => {
                 component={WeddingCollection}
               />
               <Route
+                path={routes.weddingCollectionENG}
+                component={WeddingCollection}
+              />
+              <Route
                 path={routes.underbustCollection}
                 component={UnderbustCollection}
               />
               <Route
+                path={routes.underbustCollectionENG}
+                component={UnderbustCollection}
+              />
+
+              <Route
                 path={routes.overbustCollection}
+                component={OverbustCollection}
+              />
+              <Route
+                path={routes.overbustCollectionENG}
                 component={OverbustCollection}
               />
               <Route path={routes.couture} component={Couture} />
               <Route path={routes.contact} component={Contact} />
+              <Route path={routes.contactENG} component={Contact} />
               <Route path={routes.singleProduct} component={SingleProduct} />
             </MainTemplate>
           </Switch>
