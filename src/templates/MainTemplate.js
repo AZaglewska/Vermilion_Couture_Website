0d145a4ed5cdec8607ef473b93b39a00b3f35a97
Anna Żaglewska <web.annazaglewska@gmail.com>
2021-07-06 16:37:25 +0200

    adding Footer.js to MainTemplate

--- src/templates/MainTemplate.js
+++ src/templates/MainTemplate.js
@@ -1,4 +1,5 @@
 import React from "react";
+import Footer from "../components/Navigation/Footer";
 import Navbar from "../components/Navigation/Navbar";
 
 const MainTemplate = ({ children }) => {
@@ -6,6 +7,7 @@ const MainTemplate = ({ children }) => {
     <div>
       <Navbar />
       {children}
+      <Footer />
     </div>
   );
 };
