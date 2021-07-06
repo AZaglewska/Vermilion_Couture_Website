f6d8a4884848522776339873c3d09e36d1d264af
Anna Żaglewska <web.annazaglewska@gmail.com>
2021-07-06 09:57:25 +0200

    adding Carousel from react-multi-carousel

--- src/components/ProductList.js/ProductList.js
+++ src/components/ProductList.js/ProductList.js
@@ -1,15 +1,50 @@
 import React from "react";
 import ProductElement from "../ProductElement/ProductElement";
-import { Link } from "react-router-dom";
+import Carousel from "react-multi-carousel";
 import "react-multi-carousel/lib/styles.css";
+import CustomDot from "../CarouselElements/CarouselCustomDot";
+import {
+  CustomLeftArrow,
+  CustomRightArrow,
+} from "../CarouselElements/CarouselCustomArrow";
 
-const ProductList = ({ corsetData }) => {
+const responsive = {
+  desktop: {
+    breakpoint: { max: 3000, min: 1024 },
+    items: 3,
+    paritialVisibilityGutter: 600,
+  },
+  tablet: {
+    breakpoint: { max: 1024, min: 464 },
+    items: 2,
+    paritialVisibilityGutter: 300,
+  },
+  mobile: {
+    breakpoint: { max: 650, min: 0 },
+    items: 1,
+    paritialVisibilityGutter: 40,
+  },
+};
+
+const ProductList = ({ corsetData, deviceType }) => {
   return (
-    <ul>
+    <Carousel
+      deviceType={deviceType}
+      responsive={responsive}
+      removeArrowOnDeviceType={["tablet", "mobile"]}
+      showDots={true}
+      arrows
+      infinite={true}
+      autoPlay={deviceType !== "mobile" ? true : false}
+      autoPlaySpeed={4000}
+      customDot={<CustomDot />}
+      customLeftArrow={<CustomLeftArrow />}
+      customRightArrow={<CustomRightArrow />}
+    >
       {corsetData.map((corsetDataElement) => {
         return <ProductElement corsetDataElement={corsetDataElement} />;
       })}
-    </ul>
+    </Carousel>
   );
 };
 
