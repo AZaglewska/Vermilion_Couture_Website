fab7380db5d5d9363dddb7290ad8a5a9f8c57896
Anna Żaglewska <web.annazaglewska@gmail.com>
2021-07-08 12:26:07 +0200

    adding show animations, renaming animation name, adding lineAnimation

--- src/globalStyles/Animations.js
+++ src/globalStyles/Animations.js
@@ -1,25 +1,38 @@
 import { keyframes } from "styled-components";
 
-export const zoomIn = keyframes`
-
-0% {
-  opacity: 0;
-  -webkit-transform: scale3d(.3, .3, .3);
-  transform: scale3d(.3, .3, .3);
-  }
-  50% {
-  opacity: 1;
-  }
-
-`;
-
 export const show = keyframes`
   0% {
     opacity: 0;
   }
   100% {
     opacity: 1;
-    transform: translateY(0px);
+   
   }
   
+`;
+
+export const titleAnimation = keyframes`
+  0% {
+    opacity: 0;
+    left: -60px;
+  }
+
+  100% {
+    opacity: 1;
+    left: 1%;
+  }
+
+`;
+
+export const lineAnimation = keyframes`
+  0% {
+    opacity: 0;
+    width: 0;
+  }
+
+  100% {
+    opacity: 1;
+    width: 80%;
+  }
+
 `;
