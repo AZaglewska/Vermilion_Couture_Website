6de11b0f88759fe63a7ebea3ba6e95c4e86c141c
Anna Żaglewska <web.annazaglewska@gmail.com>
2021-08-25 12:38:49 +0200

    modifying QuestionsList.js structures, adding new mapped data elements, adding styles from styled-components

--- src/components/MappedPages/QuestionsList.js
+++ src/components/MappedPages/QuestionsList.js
@@ -1,34 +1,62 @@
 import React from "react";
+import Title from "../../atoms/Title";
+import Text from "../../atoms/Text";
+import Line from "../../atoms/Line";
+import {
+  StyledQuestionsList,
+  StyledQuestionsElement,
+  StyledQuestionsBackground,
+  StyledQuestionsContent,
+  StyledQuestion,
+} from "./MappedPagesStyles/QuestionsListStyles";
 
 const QuestionsList = ({ questionsAndAnswersData }) => {
   return (
     <div>
-      <ul>
+      <StyledQuestionsList>
+        <StyledQuestionsBackground />
         {questionsAndAnswersData.map((questionsAndAnswersElement) => {
           const {
             id,
             name,
             questions,
-            images,
             articleFirst,
             articleSecond,
             articleThird,
+            articleFourth,
+            articleFifth,
+            articleSixth,
+            articleSeventh,
+            articleEighth,
           } = questionsAndAnswersElement;
 
           return (
-            <li key={id}>
-              <img src={images} />
-              <h1>{name}</h1>
-              <h2>{questions[0]}</h2>
-              <p>{articleFirst}</p>
-              <h2>{questions[1]}</h2>
-              <p>{articleSecond}</p>
-              <h2>{questions[2]}</h2>
-              <p>{articleThird}</p>
-            </li>
+            <StyledQuestionsElement key={id}>
+              <Title>{name}</Title>
+              <StyledQuestionsContent>
+                <Line />
+                <StyledQuestion first>{questions[0]}</StyledQuestion>
+                <Text>{articleFirst}</Text>
+                <StyledQuestion>{questions[1]}</StyledQuestion>
+                <Text>{articleSecond}</Text>
+                <StyledQuestion>{questions[2]}</StyledQuestion>
+                <Text>{articleThird}</Text>
+                <StyledQuestion>{questions[3]}</StyledQuestion>
+                <Text>{articleFourth}</Text>
+                <StyledQuestion>{questions[4]}</StyledQuestion>
+                <Text>{articleFifth}</Text>
+                <StyledQuestion>{questions[5]}</StyledQuestion>
+                <Text>{articleSixth}</Text>
+                <StyledQuestion>{questions[6]}</StyledQuestion>
+                <Text>{articleSeventh}</Text>
+                <StyledQuestion>{questions[7]}</StyledQuestion>
+                <Text>{articleEighth}</Text>
+                <Line second />
+              </StyledQuestionsContent>
+            </StyledQuestionsElement>
           );
         })}
-      </ul>
+      </StyledQuestionsList>
     </div>
   );
 };
