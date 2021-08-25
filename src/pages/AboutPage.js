532dc84460dddd46d154655a3b034765e733b452
Anna Żaglewska <web.annazaglewska@gmail.com>
2021-08-25 13:23:31 +0200

    modifying AboutPage.js structure, deleting current language props

--- src/pages/AboutPage.js
+++ src/pages/AboutPage.js
@@ -2,15 +2,9 @@ import React from "react";
 import About from "../components/MappedPages/About";
 import { connect } from "react-redux";
 
-const AboutPage = ({ currentLanguage, designerData }) => {
+const AboutPage = ({ designerData }) => {
   return (
     <div>
-      {currentLanguage === "PL" ? (
-        <h1>O Vermilion</h1>
-      ) : (
-        <h1>About Vermilion</h1>
-      )}
-
       <About designerData={designerData} />
     </div>
   );
@@ -18,7 +12,6 @@ const AboutPage = ({ currentLanguage, designerData }) => {
 
 const mapStateToProps = (state) => ({
   designerData: state.designerData,
-  currentLanguage: state.currentLanguage,
 });
 
 export default connect(mapStateToProps)(AboutPage);
