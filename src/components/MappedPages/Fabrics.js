7138e86753b7d11b680ce4eab045f8c53beaa37c
Anna Żaglewska <web.annazaglewska@gmail.com>
2021-08-25 12:27:48 +0200

    modifying Fabrics.js structures, importing styles from styled-components, adding currentLanguage props

--- src/components/MappedPages/Fabrics.js
+++ src/components/MappedPages/Fabrics.js
@@ -1,22 +1,45 @@
 import React from "react";
+import Title from "../../atoms/Title";
+import Text from "../../atoms/Text";
+import Line from "../../atoms/Line";
+import {
+  StyledFabricsWrapper,
+  StyledFabricsBackground,
+  StyledFabricsContent,
+  StyledFabricsListGrid,
+  StyledFabricsElement,
+  StyledFabricsImage,
+  StyledFabricsName,
+} from "./MappedPagesStyles/FabricsStyles";
 
-const Fabrics = ({ fabricsData }) => {
+const Fabrics = ({ fabricsData, currentLanguage }) => {
   return (
-    <div>
-      <ul>
-        {fabricsData.map((fabricsDataElement) => {
-          const { id, name, images, description } = fabricsDataElement;
-
-          return (
-            <li key={id}>
-              <h1>{name}</h1>
-              <img src={images} />
-              <p>{description}</p>
-            </li>
-          );
-        })}
-      </ul>
-    </div>
+    <StyledFabricsWrapper>
+      <StyledFabricsBackground />
+      <StyledFabricsContent>
+        <Title> {currentLanguage === "PL" ? "Tkaniny" : "Fabrics"} </Title>
+        <Line />
+        <Text styleType="biggerText">
+          {currentLanguage === "PL"
+            ? "Poniżej przedstawiamy Wam najpopularniejsze tkaniny z których chętnie uszyjemy Wam wymarzony gorset:"
+            : "Below we present the most popular fabrics, that we are happy to sew your dream corset from:"}
+        </Text>
+        <StyledFabricsListGrid>
+          {fabricsData.map((fabricsDataElement) => {
+            const { id, name, images, description } = fabricsDataElement;
+            return (
+              <StyledFabricsElement key={id}>
+                <StyledFabricsName>{name}</StyledFabricsName>
+                <Line styleType="greyLine" />
+                <StyledFabricsImage src={images} />
+                <Text>{description}</Text>
+              </StyledFabricsElement>
+            );
+          })}
+        </StyledFabricsListGrid>
+        <Line second />
+      </StyledFabricsContent>
+    </StyledFabricsWrapper>
   );
 };
 
