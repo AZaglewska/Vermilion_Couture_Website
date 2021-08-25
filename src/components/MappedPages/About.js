b48642d2f73350378323a6844a4706b78fc8eb6c
Anna Żaglewska <web.annazaglewska@gmail.com>
2021-08-25 12:24:43 +0200

    modifying About.js structures, mapping designerData

--- src/components/MappedPages/About.js
+++ src/components/MappedPages/About.js
@@ -2,11 +2,11 @@ import React from "react";
 import Title from "../../atoms/Title";
 import Text from "../../atoms/Text";
 import Line from "../../atoms/Line";
+import Image from "../../atoms/Image";
 import {
   StyledAboutList,
   StyledAboutContainer,
   StyledAboutElements,
-  StyledAboutImg,
   StyledTitleContainer,
 } from "./MappedPagesStyles/AboutStyles";
 
@@ -15,26 +15,52 @@ const About = ({ designerData }) => {
     <div>
       <ul>
         {designerData.map((designerDataElement) => {
-          const { id, name, images, articleFirst, articleSecond } =
-            designerDataElement;
+          const {
+            id,
+            name,
+            nameSecond,
+            images,
+            articleFirst,
+            articleSecond,
+            articleThird,
+            articleFourth,
+            articleFifth,
+            articleSixth,
+          } = designerDataElement;
 
           return (
             <StyledAboutList key={id}>
               <StyledAboutContainer>
-                <StyledAboutImg src={images[0]} />
+                <Image styleType="about" src={images[0]} alt={name} logo />
                 <StyledAboutElements>
                   <StyledTitleContainer>
                     <Title>{name}</Title>
                   </StyledTitleContainer>
                   <Text>{articleFirst}</Text>
+                  <Text marginTop>{articleSecond}</Text>
                 </StyledAboutElements>
               </StyledAboutContainer>
               <Line styleType="about" />
               <StyledAboutContainer>
                 <StyledAboutElements>
-                  <Text>{articleSecond}</Text>
+                  <StyledTitleContainer>
+                    <Title>{nameSecond[0]}</Title>
+                  </StyledTitleContainer>
+                  <Text>{articleThird}</Text>
+                  <Text marginTop>{articleFourth}</Text>
+                  <Text marginTop>{articleFifth}</Text>
                 </StyledAboutElements>
-                <StyledAboutImg src={images[1]} />
+                <Image styleType="about" src={images[1]} alt={name} designer />
+              </StyledAboutContainer>
+              <Line styleType="about" />
+              <StyledAboutContainer>
+                <Image styleType="about" src={images[2]} alt={name} />
+                <StyledAboutElements>
+                  <StyledTitleContainer>
+                    <Title>{nameSecond[1]}</Title>
+                  </StyledTitleContainer>
+                  <Text>{articleSixth}</Text>
+                </StyledAboutElements>
               </StyledAboutContainer>
             </StyledAboutList>
           );
