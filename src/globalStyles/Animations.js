c35512eeebd7c5d22208bca6eacad464a40f53f1
Anna Żaglewska <web.annazaglewska@gmail.com>
2021-08-31 20:24:13 +0200

    modifying Animation.js

--- src/globalStyles/Animations.js
+++ src/globalStyles/Animations.js
@@ -10,6 +10,36 @@ export const show = keyframes`
   }
   
 `;
+export const showAnimatedLogoComponent = keyframes`
+  0% {
+    height: 0;
+  }
+
+  100% {
+    height: 100%;
+  }
+
+`;
+export const animatedLogoPaths = keyframes`
+    from {
+      fill-opacity: 0%;
+    }
+    to {
+      fill-opacity: 100%;
+    }
+  
+`;
+
+export const colorChange = keyframes`
+    from {
+      fill:#353530;
+    }
+    to {
+  
+     fill:#dbc09a;
+    }
+
+`;
 
 export const titleAnimation = keyframes`
   0% {
@@ -25,32 +55,6 @@ export const titleAnimation = keyframes`
 `;
 
 export const lineAnimation = keyframes`
-  0% {
-    opacity: 0;
-    width: 0;
-  }
-
-  100% {
-    opacity: 1;
-    width: 68%;
-  }
-
-`;
-
-export const lineAnimation2 = keyframes`
-  0% {
-    opacity: 0;
-    width: 0;
-  }
-
-  100% {
-    opacity: 1;
-    width: 90%;
-  }
-
-`;
-
-export const lineAnimation3 = keyframes`
   0% {
     width: 0;
   }
