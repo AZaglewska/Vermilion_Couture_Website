aede14febf107bff3fa6edd1aa830700fd4f74b1
Anna Żaglewska <web.annazaglewska@gmail.com>
2021-08-31 13:49:55 +0200

    modifying page structures and about styles, adding medie queries

--- src/components/MappedPages/About.js
+++ src/components/MappedPages/About.js
@@ -6,19 +6,20 @@ import Image from "../../atoms/Image";
 import {
   StyledAboutList,
   StyledAboutContainer,
+  StyledAboutContent,
+  StyledAboutTitleWrapper,
   StyledAboutElements,
-  StyledTitleContainer,
+  StyledAboutTextContainer,
 } from "./MappedPagesStyles/AboutStyles";
 
 const About = ({ designerData }) => {
   return (
-    <div>
+    <>
       <ul>
         {designerData.map((designerDataElement) => {
           const {
             id,
             name,
-            nameSecond,
             images,
             articleFirst,
             articleSecond,
@@ -31,42 +32,56 @@ const About = ({ designerData }) => {
           return (
             <StyledAboutList key={id}>
               <StyledAboutContainer>
-                <Image styleType="about" src={images[0]} alt={name} logo />
-                <StyledAboutElements>
-                  <StyledTitleContainer>
-                    <Title>{name}</Title>
-                  </StyledTitleContainer>
-                  <Text>{articleFirst}</Text>
-                  <Text marginTop>{articleSecond}</Text>
-                </StyledAboutElements>
+                <StyledAboutContent>
+                  <Image src={images[0]} alt={name} />
+                  <StyledAboutTextContainer>
+                    <StyledAboutTitleWrapper>
+                      <Title>{name}</Title>
+                      <Line />
+                    </StyledAboutTitleWrapper>
+                    <Text styleType="about">{articleFirst}</Text>
+                    <Text styleType="about" marginTop>
+                      {articleSecond}
+                    </Text>
+                  </StyledAboutTextContainer>
+                </StyledAboutContent>
               </StyledAboutContainer>
-              <Line styleType="about" />
               <StyledAboutContainer>
                 <StyledAboutElements>
-                  <StyledTitleContainer>
-                    <Title>{nameSecond[0]}</Title>
-                  </StyledTitleContainer>
-                  <Text>{articleThird}</Text>
-                  <Text marginTop>{articleFourth}</Text>
-                  <Text marginTop>{articleFifth}</Text>
+                  <Line />
                 </StyledAboutElements>
-                <Image styleType="about" src={images[1]} alt={name} designer />
+                <StyledAboutContent second>
+                  <StyledAboutTextContainer>
+                    <Text styleType="about">{articleThird}</Text>
+                    <Text styleType="about" marginTop>
+                      {articleFourth}
+                    </Text>
+                    <Text styleType="about" marginTop>
+                      {articleFifth}
+                    </Text>
+                  </StyledAboutTextContainer>
+                  <Image styleType="about" src={images[1]} alt={name} />
+                </StyledAboutContent>
               </StyledAboutContainer>
-              <Line styleType="about" />
               <StyledAboutContainer>
-                <Image styleType="about" src={images[2]} alt={name} />
                 <StyledAboutElements>
-                  <StyledTitleContainer>
-                    <Title>{nameSecond[1]}</Title>
-                  </StyledTitleContainer>
-                  <Text>{articleSixth}</Text>
+                  <Line />
+                </StyledAboutElements>
+                <StyledAboutContent third>
+                  <Image styleType="about" src={images[2]} alt={name} />
+                  <StyledAboutTextContainer>
+                    <Text styleType="about">{articleSixth}</Text>
+                  </StyledAboutTextContainer>
+                </StyledAboutContent>
+                <StyledAboutElements>
+                  <Line />
                 </StyledAboutElements>
               </StyledAboutContainer>
             </StyledAboutList>
           );
         })}
       </ul>
-    </div>
+    </>
   );
 };
 
--- src/components/MappedPages/MappedPagesStyles/AboutStyles.js
+++ src/components/MappedPages/MappedPagesStyles/AboutStyles.js
@@ -1,28 +1,54 @@
 import styled from "styled-components";
+import { show } from "../../../globalStyles/Animations";
 
 export const StyledAboutList = styled.li`
   display: flex;
   flex-direction: column;
   align-items: center;
-  margin-top: 100px;
+  width: 100%;
+  padding: 40px 0;
+  animation: ${show} 0.1s ease forwards;
+  animation-duration: 3s;
 `;
 
 export const StyledAboutContainer = styled.div`
   display: flex;
+  flex-direction: column;
   justify-content: center;
   align-items: center;
+  width: 100%;
+`;
+
+export const StyledAboutContent = styled.div`
+  display: flex;
+  justify-content: space-evenly;
+  align-items: center;
+  margin: ${({ theme }) => theme.margins.l} 0;
+  width: ${({ third }) => (third ? "60%" : "80%")};
+
+  @media (max-width: 1024px) {
+    flex-direction: ${({ second }) => (second ? "column-reverse" : "column")};
+  }
+  @media (max-width: 540px) {
+    width: 90%;
+  }
+`;
+export const StyledAboutTitleWrapper = styled.div`
+  margin-bottom: ${({ theme }) => theme.margins.m};
 `;
 
 export const StyledAboutElements = styled.div`
-  width: 40%;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  flex-direction: column;
-  margin: 0 ${({ theme }) => theme.margins.xl};
+  width: 80%;
+  text-align: center;
 `;
 
-export const StyledTitleContainer = styled.div`
-  align-self: flex-start;
-  margin-bottom: ${({ theme }) => theme.margins.l};
+export const StyledAboutTextContainer = styled.div`
+  margin: 0 ${({ theme }) => theme.margins.m};
+  display: flex;
+  flex-direction: column;
+  width: 60%;
+  @media (max-width: 1024px) {
+    width: 90%;
+    margin: ${({ theme }) => theme.margins.l} 0 0 0;
+  }
 `;
