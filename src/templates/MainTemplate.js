b2d704492700b06d291fdc515de285b3ee390b63
Anna Żaglewska <web.annazaglewska@gmail.com>
2021-08-31 14:17:02 +0200

    modifying MainTemplate.js, adding setTimeout fn and showAndHideArrowTop fn

--- src/templates/MainTemplate.js
+++ src/templates/MainTemplate.js
@@ -1,15 +1,47 @@
-import React from "react";
+import React, { useState, useEffect, useContext } from "react";
+import AnimatedLogo from "../components/AnimatedLogo/AnimatedLogo";
 import Footer from "../components/Navigation/Footer";
 import Navbar from "../components/Navigation/Navbar";
 import OfferCard from "../components/OfferCard/OfferCard";
+import StickyMenu from "../components/StickyMenu/StickyMenu";
+import ArrowUp from "../components/IconComponents/ArrowUp/ArrowUp";
 
 const MainTemplate = ({ children }) => {
+  const [isShown, setIsShown] = useState(false);
+  const [showArrowTop, setShowArrowTop] = useState(false);
+
+  const showAndHideArrowTop = () => {
+    if (!showArrowTop && window.pageYOffset < 500) {
+      setShowArrowTop(true);
+    } else if (showArrowTop && window.pageYOffset >= 500) {
+      setShowArrowTop(false);
+    }
+  };
+  window.addEventListener("scroll", showAndHideArrowTop);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setIsShown(!isShown);
+    }, 7200);
+  }, []);
+
   return (
     <>
-      <Navbar />
-      {children}
-      <OfferCard />
-      <Footer />
+      {/* <AnimatedLogo isShown={isShown} /> */}
+      {isShown ? (
+        <>
+          <Navbar />
+          {children}
+          <OfferCard />
+          <Footer />
+          <StickyMenu />
+          {showArrowTop === false ? <ArrowUp /> : null}
+        </>
+      ) : (
+        <>
+          <AnimatedLogo isShown={isShown} />
+        </>
+      )}
     </>
   );
 };
