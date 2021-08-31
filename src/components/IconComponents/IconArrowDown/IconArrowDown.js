629434156c239f13cc698cc38ee823a5b979c5bf
Anna Żaglewska <web.annazaglewska@gmail.com>
2021-08-31 13:45:36 +0200

    modifying StyledIconArrowDown styles

--- src/components/IconComponents/IconArrowDown/IconArrowDown.js
+++ src/components/IconComponents/IconArrowDown/IconArrowDown.js
@@ -3,15 +3,16 @@ import styled from "styled-components";
 import { IoIosArrowDown } from "react-icons/io";
 
 export const StyledIconArrowDown = styled(IoIosArrowDown)`
-  width: 15px;
-  display: ${(props) => (props.dropdownOpen ? "none" : "inline-block")};
-  margin: ${(props) => (props.nav ? "-2px 3px" : "0px 3px")};
+  display: ${({ dropdownOpen }) => (dropdownOpen ? "none" : "inline-block")};
+  margin: ${({ nav }) => (nav === "true" ? "-2px 3px" : "0px 4px")};
 `;
 
 const IconArrowDown = (props) => {
-  const { dropdownOpen, nav } = props;
+  const { dropdownOpen, nav, size } = props;
 
-  return <StyledIconArrowDown dropdownOpen={dropdownOpen} nav={nav} />;
+  return (
+    <StyledIconArrowDown dropdownOpen={dropdownOpen} nav={nav} size={size} />
+  );
 };
 
 export default IconArrowDown;
