6ceb24b3c8e2e5618e113856ee79419234eeb762
Anna Żaglewska <web.annazaglewska@gmail.com>
2021-09-01 20:11:04 +0200

    adding media queries, privacyPolicy prop

--- src/atoms/Text.js
+++ src/atoms/Text.js
@@ -5,27 +5,34 @@ const StyledText = styled.p`
   color: ${({ theme }) => theme.colors.lightGrey};
   font-size: ${({ theme }) => theme.fontSizes.s};
   line-height: 25px;
+  @media (max-width: 320px) {
+    font-size: ${({ theme }) => theme.fontSizes.xs};
+  }
 
   ${({ styleType }) =>
     styleType === "about" &&
     css`
       margin-top: ${({ marginTop }) => (marginTop ? "15px" : "none")};
-      @media (max-width: 320px) {
-        font-size: ${({ theme }) => theme.fontSizes.xs};
-      }
     `}
 
   ${({ styleType }) =>
     styleType === "biggerText" &&
     css`
-      margin: ${({ theme }) => theme.margins.l} 0;
+      margin: ${({ theme }) => theme.margins.m} 0;
       font-size: 17px;
+      @media (max-width: 320px) {
+        font-size: ${({ privacyPolicy }) => (privacyPolicy ? "12px" : "15px")};
+      }
     `}
 `;
 
-const Text = ({ children, marginTop, styleType }) => {
+const Text = ({ children, marginTop, privacyPolicy, styleType }) => {
   return (
-    <StyledText marginTop={marginTop} styleType={styleType}>
+    <StyledText
+      marginTop={marginTop}
+      privacyPolicy={privacyPolicy}
+      styleType={styleType}
+    >
       {children}
     </StyledText>
   );
