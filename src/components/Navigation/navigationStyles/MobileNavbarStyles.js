69f61a13c31338c3ae80f7fcfca0833ea183441e
Anna Żaglewska <web.annazaglewska@gmail.com>
2021-09-01 23:17:50 +0200

    adding MobileNavbarStyles.js

---
+++ src/components/Navigation/navigationStyles/MobileNavbarStyles.js
@@ -0,0 +1,150 @@
+import styled from "styled-components";
+import { Link } from "react-router-dom";
+import { StyledLogo } from "../../IconComponents/LogoIcon/LogoIconStyles";
+import { StyledIconArrowDown } from "../../IconComponents/IconArrowDown/IconArrowDown";
+import { StyledIconArrowUp } from "../../IconComponents/IconArrowUp/IconArrowUp";
+import { show } from "../../../globalStyles/Animations";
+
+export const MobileNavContent = styled.ul`
+  display: none;
+  @media (max-width: 768px) {
+    overflow-x: hidden;
+    z-index: 15;
+    list-style: none;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    background-color: white;
+    position: fixed;
+    transform: ${({ hamburgerMenuOpen }) =>
+      hamburgerMenuOpen ? "translateY(0)" : "translateY(-100%)"};
+    top: 0;
+    right: 0;
+    height: 100%;
+    width: 100%;
+    transition: transform 0.3s ease-in-out;
+  }
+`;
+export const MobileIconContent = styled.div`
+  border-bottom: 1px solid ${({ theme }) => theme.colors.snowGrey};
+  display: flex;
+  align-items: center;
+  padding: 10px 20px;
+  width: 100%;
+  margin-bottom: ${({ theme }) => theme.margins.m};
+  height: 20vh;
+`;
+
+export const MobileIconWrapper = styled.div`
+  width: ${({ flag }) => (flag ? "20%" : "60%")};
+  &:hover {
+    ${StyledLogo} {
+      fill: ${({ theme }) => theme.colors.goldBeige};
+    }
+  }
+`;
+
+export const MobileDropdown = styled.ul`
+  position: relative;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  height: 100%;
+`;
+export const MobileDropbtn = styled.button`
+  color: ${({ theme }) => theme.colors.dark};
+  background-color: transparent;
+  border: none;
+  position: relative;
+  font-size: ${({ theme }) => theme.fontSizes.xl};
+  margin: ${({ contact }) => (contact ? "5px 16px 0 0" : "")};
+
+  @media (max-width: 540px) {
+    font-size: ${({ theme }) => theme.fontSizes.m};
+  }
+
+  @media (max-width: 360px) {
+    font-size: ${({ theme }) => theme.fontSizes.s};
+  }
+
+  &:after {
+    content: "";
+    display: block;
+    width: 0;
+    height: 1px;
+    background-color: ${({ theme }) => theme.colors.goldBeige};
+    transition: width 0.3s;
+    position: absolute;
+    top: 108%;
+  }
+`;
+
+export const MobileDropbtnLink = styled(Link)`
+  text-decoration: none;
+  color: ${({ theme }) => theme.colors.dark};
+`;
+export const MobileButtonWrapper = styled.div`
+  display: flex;
+  align-items: center;
+  margin: ${({ dropdownOpen }) => (dropdownOpen ? "5px" : "20px")};
+
+  &:hover,
+  &:focus {
+    ${StyledIconArrowDown} {
+      color: ${({ theme }) => theme.colors.goldBeige};
+    }
+
+    ${StyledIconArrowUp} {
+      color: ${({ theme }) => theme.colors.goldBeige};
+    }
+
+    ${MobileDropbtn} {
+      color: ${({ theme }) => theme.colors.goldBeige};
+      :after {
+        width: 100%;
+        transition: 0.3s;
+      }
+    }
+    ${MobileDropbtnLink} {
+      color: ${({ theme }) => theme.colors.goldBeige};
+    }
+  }
+`;
+
+export const MobileDropdownContent = styled.div`
+  display: ${({ dropdownOpen }) => (dropdownOpen ? "flex" : "none")};
+  flex-direction: column;
+  text-align: center;
+`;
+
+export const MobileDropdownContentLink = styled(Link)`
+  text-decoration: none;
+  color: ${({ theme }) => theme.colors.lightGrey};
+  font-size: ${({ theme }) => theme.fontSizes.l};
+  animation: ${show} 0.1s ease forwards;
+  animation-duration: 1s;
+  transition: ease 1s;
+  margin: ${({ theme }) => theme.margins.xs} 0;
+
+  &:hover,
+  &:focus {
+    color: ${({ theme }) => theme.colors.snowGrey};
+  }
+
+  @media (max-width: 540px) {
+    font-size: ${({ theme }) => theme.fontSizes.s};
+  }
+  @media (max-width: 360px) {
+    font-size: ${({ theme }) => theme.fontSizes.xs};
+  }
+`;
+
+export const MobileFooter = styled.div`
+  border-top: 1px solid ${({ theme }) => theme.colors.snowGrey};
+  height: 10vh;
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+  width: 100%;
+`;
