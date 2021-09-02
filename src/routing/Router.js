b01d81c297bedf1ddba40cbaa06e29aacd5d3727
Anna Żaglewska <web.annazaglewska@gmail.com>
2021-09-03 01:25:26 +0200

    adding ScrollToTop fn, adding ScrollToTop fn to Router.js

--- src/routing/Router.js
+++ src/routing/Router.js
@@ -1,6 +1,7 @@
 import React from "react";
 import MainTemplate from "../templates/MainTemplate";
 import Home from "../pages/Home";
+import ScrollToTop from "../components/ScrollToTop/ScrollToTop";
 import AboutPage from "../pages/AboutPage";
 import EveningCollection from "../pages/EveningCollection";
 import WeddingCollection from "../pages/WeddingCollection";
@@ -28,94 +29,102 @@ const Router = () => {
     <div>
       <BrowserRouter>
         <ThemeProvider theme={mainTheme}>
-          <Switch>
-            <MainTemplate>
-              <Route exact path={routes.home} component={Home} />
-              <Route path={routes.about} component={AboutPage} />
-              <Route path={routes.aboutENG} component={AboutPage} />
-              <Route
-                path={routes.eveningCollection}
-                component={EveningCollection}
-              />
-              <Route
-                path={routes.eveningCollectionENG}
-                component={EveningCollection}
-              />
-              <Route
-                path={routes.weddingCollection}
-                component={WeddingCollection}
-              />
-              <Route
-                path={routes.weddingCollectionENG}
-                component={WeddingCollection}
-              />
-              <Route
-                path={routes.underbustCollection}
-                component={UnderbustCollection}
-              />
-              <Route
-                path={routes.underbustCollectionENG}
-                component={UnderbustCollection}
-              />
-              <Route
-                path={routes.overbustCollection}
-                component={OverbustCollection}
-              />
-              <Route
-                path={routes.overbustCollectionENG}
-                component={OverbustCollection}
-              />
-              <Route path={routes.meshCollection} component={MeshCollection} />
-              <Route
-                path={routes.meshCollectionENG}
-                component={MeshCollection}
-              />
-              <Route path={routes.silkCollection} component={SilkCollection} />
-              <Route
-                path={routes.silkCollectionENG}
-                component={SilkCollection}
-              />
-              <Route
-                path={routes.otherCollection}
-                component={OtherCollection}
-              />
-              <Route
-                path={routes.otherCollectionENG}
-                component={OtherCollection}
-              />
-              <Route path={routes.contact} component={Contact} />
-              <Route path={routes.contactENG} component={Contact} />
-              <Route path={routes.singleProduct} component={SingleProduct} />
-              <Route
-                path={routes.askedQuestions}
-                component={AskedQuestionsPage}
-              />
-              <Route
-                path={routes.askedQuestionsENG}
-                component={AskedQuestionsPage}
-              />
-              <Route path={routes.fabrics} component={FabricsPage} />
-              <Route path={routes.fabricsENG} component={FabricsPage} />
-              <Route path={routes.measuring} component={MeasuringPage} />
-              <Route path={routes.measuringENG} component={MeasuringPage} />
-              <Route
-                path={routes.aboutCollection}
-                component={AboutCollectionPage}
-              />
-              <Route
-                path={routes.aboutCollectionENG}
-                component={AboutCollectionPage}
-              />
-              <Route
-                path={routes.privacyPolicyPL}
-                component={PrivacyPolicyPL}
-              />
-              <Route
-                path={routes.privacyPolicyENG}
-                component={PrivacyPolicyENG}
-              />
-            </MainTemplate>
-          </Switch>
+          <ScrollToTop>
+            <Switch>
+              <MainTemplate>
+                <Route exact path={routes.home} component={Home} />
+                <Route path={routes.about} component={AboutPage} />
+                <Route path={routes.aboutENG} component={AboutPage} />
+                <Route
+                  path={routes.eveningCollection}
+                  component={EveningCollection}
+                />
+                <Route
+                  path={routes.eveningCollectionENG}
+                  component={EveningCollection}
+                />
+                <Route
+                  path={routes.weddingCollection}
+                  component={WeddingCollection}
+                />
+                <Route
+                  path={routes.weddingCollectionENG}
+                  component={WeddingCollection}
+                />
+                <Route
+                  path={routes.underbustCollection}
+                  component={UnderbustCollection}
+                />
+                <Route
+                  path={routes.underbustCollectionENG}
+                  component={UnderbustCollection}
+                />
+                <Route
+                  path={routes.overbustCollection}
+                  component={OverbustCollection}
+                />
+                <Route
+                  path={routes.overbustCollectionENG}
+                  component={OverbustCollection}
+                />
+                <Route
+                  path={routes.meshCollection}
+                  component={MeshCollection}
+                />
+                <Route
+                  path={routes.meshCollectionENG}
+                  component={MeshCollection}
+                />
+                <Route
+                  path={routes.silkCollection}
+                  component={SilkCollection}
+                />
+                <Route
+                  path={routes.silkCollectionENG}
+                  component={SilkCollection}
+                />
+                <Route
+                  path={routes.otherCollection}
+                  component={OtherCollection}
+                />
+                <Route
+                  path={routes.otherCollectionENG}
+                  component={OtherCollection}
+                />
+                <Route path={routes.contact} component={Contact} />
+                <Route path={routes.contactENG} component={Contact} />
+                <Route path={routes.singleProduct} component={SingleProduct} />
+                <Route
+                  path={routes.askedQuestions}
+                  component={AskedQuestionsPage}
+                />
+                <Route
+                  path={routes.askedQuestionsENG}
+                  component={AskedQuestionsPage}
+                />
+                <Route path={routes.fabrics} component={FabricsPage} />
+                <Route path={routes.fabricsENG} component={FabricsPage} />
+                <Route path={routes.measuring} component={MeasuringPage} />
+                <Route path={routes.measuringENG} component={MeasuringPage} />
+                <Route
+                  path={routes.aboutCollection}
+                  component={AboutCollectionPage}
+                />
+                <Route
+                  path={routes.aboutCollectionENG}
+                  component={AboutCollectionPage}
+                />
+                <Route
+                  path={routes.privacyPolicyPL}
+                  component={PrivacyPolicyPL}
+                />
+                <Route
+                  path={routes.privacyPolicyENG}
+                  component={PrivacyPolicyENG}
+                />
+              </MainTemplate>
+            </Switch>
+          </ScrollToTop>
         </ThemeProvider>
       </BrowserRouter>
     </div>
