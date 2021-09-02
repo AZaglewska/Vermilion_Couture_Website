bc9449171d6d240f30d758caedbf403ea5353b16
Anna Żaglewska <web.annazaglewska@gmail.com>
2021-09-02 13:54:22 +0200

    adding margin-bottom, adding on hover colors

--- src/components/IconComponents/IconList/IconListStyles.js
+++ src/components/IconComponents/IconList/IconListStyles.js
@@ -1,19 +1,18 @@
 import styled from "styled-components";
 
-export const IconLink = styled.a`
-  margin-bottom: ${({ footer }) => (footer ? "8px" : "0px")};
-`;
+export const IconLink = styled.a``;
 
 export const Icon = styled.svg`
   width: 35px;
   height: 20px;
   fill: ${({ theme }) => theme.colors.dark};
   transition: fill 0.5s;
+  margin-bottom: ${({ footer }) => (footer ? "8px" : "0px")};
   @media (max-width: 320px) {
     width: ${({ footer }) => (footer ? "35px" : "30px")};
     height: ${({ footer }) => (footer ? "20px" : "17px")};
   }
-  :hover {
-    fill: ${({ theme }) => theme.colors.goldBeige};
+  &:hover {
+    fill: ${({ footer }) => (footer ? "#b3aeaa" : "#d9c7b0")};
   }
 `;
